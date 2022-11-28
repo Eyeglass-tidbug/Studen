5bf44596fede7e1f011e9d4c422d991498f430a6
Eyeglass-tidbug <113344625+Eyeglass-tidbug@users.noreply.github.com>
2022-11-28 22:34:50 +0700

    test1

---
+++ lap00pa/lap00pa/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+internal class Studen
+{
+    public string Name;
+    public float Weight
+}
+
+Studen studen1 = new Studen();
+studen1.Name = "nantawat"
+ studen1.Weight = 63
+
+Studen studen2 = new Studen();
+studen2.Name = "noppasin"
+ studen2.Weight = 62
+
+Studen studen3 = new Studen();
+studen3.Name = "naphat"
+ studen3.Weight = 90
+
+Studen studen4 = new Studen();
+studen4.Name = "nantipat"
+ studen4.Weight = 61
+
+Studen studen5 = new.Studen();
+studen5.Name = "kitchawakon"
+ studen5.Weight = 60
+
+//console.WriteLine((studen1.Weight + studen2.Weight + studen3.Weight + studen4.Weight + studen5.Weight)/5)
+console.WriteLine("hello")
